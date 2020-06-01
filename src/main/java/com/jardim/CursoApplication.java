e12eb6df57e17618684f0c8680c049f464006f9a
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-01 17:55:07 -0300

    criando macete para instanciar categorias

--- src/main/java/com/jardim/CursoApplication.java
+++ src/main/java/com/jardim/CursoApplication.java
@@ -1,13 +1,32 @@
 package com.jardim;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoApplication {
+import com.jardim.domain.Categoria;
+import com.jardim.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+	}
+	
+	
 }
