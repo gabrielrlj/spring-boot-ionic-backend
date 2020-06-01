766c0ee0af94bde06b223892636c5295211f4ae0
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-01 18:18:04 -0300

    produto e associacao muitos para muitos

--- src/main/java/com/jardim/CursoApplication.java
+++ src/main/java/com/jardim/CursoApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.jardim.domain.Categoria;
+import com.jardim.domain.Produto;
 import com.jardim.repositories.CategoriaRepository;
+import com.jardim.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursoApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepo;
+	@Autowired
+	private ProdutoRepository produtoRepo;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
@@ -25,7 +29,20 @@ public class CursoApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 30.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepo.saveAll(Arrays.asList(p1, p2, p3));
 	}
 	
 	
