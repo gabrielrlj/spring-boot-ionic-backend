e6a3ecf9001a706b832457f56c98326e8e3a7f12
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-03 14:31:16 -0300

    endpoint /pedidos/id disponibilizado

--- src/main/java/com/jardim/domain/ItemPedido.java
+++ src/main/java/com/jardim/domain/ItemPedido.java
@@ -5,12 +5,15 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	//criar chave composta com o produto e o pedido
 	//id embutido em um tipo auxiliar
+	@JsonIgnore //para impedir de serializar este atributo, esta chave apenas faz sentido no progmram
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -31,9 +34,11 @@ public class ItemPedido implements Serializable{
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
+	
 	
 	public Produto getProduto() {
 		return id.getProduto();
