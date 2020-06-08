6de16139bab2210490b7d5e1c995780626bcdcc9
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-08 14:20:34 -0300

    put delete e get para cliente

--- src/main/java/com/jardim/resources/ClienteResource.java
+++ src/main/java/com/jardim/resources/ClienteResource.java
@@ -1,16 +1,24 @@
 package com.jardim.resources;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jardim.domain.Cliente;
+import com.jardim.dto.ClienteDTO;
 import com.jardim.services.ClienteService;
 
 @RestController
@@ -26,5 +34,43 @@ public class ClienteResource {
 		
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@PutMapping(value="/{id}")
+	public ResponseEntity<Void> atualiza(@PathVariable Integer id, @Valid @RequestBody ClienteDTO objDto){
+		
+		Cliente obj = service.fromDto(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> deletarPorId(@PathVariable Integer id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	//rota = /categorias/page?page=0&linesPerPage=20...
+	@GetMapping(value="/page")
+	public ResponseEntity<Page<ClienteDTO>> listarPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> listar() {
+		List<Cliente> list = service.buscar();
+		//percorrendo a lista de categorias e transpondo-a para uma lista de categoriasdto
+		List<ClienteDTO> listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
 
 }
--- src/main/java/com/jardim/services/ClienteService.java
+++ src/main/java/com/jardim/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.jardim.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jardim.domain.Cliente;
+import com.jardim.dto.ClienteDTO;
 import com.jardim.repositories.ClienteRepository;
+import com.jardim.services.exceptions.DataIntegrityException;
 import com.jardim.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,5 +25,44 @@ public class ClienteService {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = buscarPorId(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public void delete(Integer id) {
+		buscarPorId(id);
+		try {
+			repo.deleteById(id);	
+		}catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possível excluir há entidades relacionadas!");
+		}
+		
+	}
+
+	public List<Cliente> buscar() {
+		
+		return repo.findAll();
+	}
+	
+	//retorna um página de dados de categorias
+	//parâmetros em ordem: a página, quantas linhas por página, ordenação, asc ou desc
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		//objeto que prepara minhas informações para que eu faça a consulta que me retorna a pagina de dados
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	//méotodo auxiliar que instancia uma categoria a partir de um dto
+	public Cliente fromDto(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
 }
