9d635f10372b9708df605fff829599bed2ea4277
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-08 18:49:02 -0300

    validacao customizada de email não existente na atualizacao de cliente

--- src/main/java/com/jardim/dto/ClienteDTO.java
+++ src/main/java/com/jardim/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.jardim.domain.Cliente;
+import com.jardim.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
