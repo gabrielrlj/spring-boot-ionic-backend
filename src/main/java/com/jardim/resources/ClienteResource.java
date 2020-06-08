709d1d4939f5c2fcd9dd004ce31bec66993e18ad
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-08 15:09:31 -0300

    inserindo um novo cliente com post

--- src/main/java/com/jardim/resources/ClienteResource.java
+++ src/main/java/com/jardim/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.jardim.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,14 +12,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jardim.domain.Cliente;
 import com.jardim.dto.ClienteDTO;
+import com.jardim.dto.NewClienteDTO;
 import com.jardim.services.ClienteService;
 
 @RestController
@@ -33,6 +37,18 @@ public class ClienteResource {
 		Cliente obj = service.buscarPorId(id);
 		
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> inserir(@Valid @RequestBody NewClienteDTO objDto){
+		Cliente obj = service.fromDto(objDto);
+		obj = service.insert(obj);
+		//aparentemente gera o código http 201 (created) e gera a url do novo recurso criado
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@PutMapping(value="/{id}")
--- src/main/java/com/jardim/services/ClienteService.java
+++ src/main/java/com/jardim/services/ClienteService.java
@@ -3,6 +3,8 @@ package com.jardim.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.jardim.domain.Cidade;
 import com.jardim.domain.Cliente;
+import com.jardim.domain.Endereco;
+import com.jardim.domain.enums.TipoCliente;
 import com.jardim.dto.ClienteDTO;
+import com.jardim.dto.NewClienteDTO;
 import com.jardim.repositories.ClienteRepository;
+import com.jardim.repositories.EnderecoRepository;
 import com.jardim.services.exceptions.DataIntegrityException;
 import com.jardim.services.exceptions.ObjectNotFoundException;
 
@@ -20,11 +27,22 @@ import com.jardim.services.exceptions.ObjectNotFoundException;
 public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
-
+	
+	@Autowired
+	private EnderecoRepository enderecoRepo;
+	
 	public Cliente buscarPorId(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepo.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -53,7 +71,7 @@ public class ClienteService {
 		return repo.findAll();
 	}
 	
-	//retorna um página de dados de categorias
+	//retorna um página de dados de clientes
 	//parâmetros em ordem: a página, quantas linhas por página, ordenação, asc ou desc
 	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		//objeto que prepara minhas informações para que eu faça a consulta que me retorna a pagina de dados
@@ -61,7 +79,31 @@ public class ClienteService {
 		return repo.findAll(pageRequest);
 	}
 	
-	//méotodo auxiliar que instancia uma categoria a partir de um dto
+	public Cliente fromDto(NewClienteDTO objDto) {
+		
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), 
+				TipoCliente.toEnum(objDto.getTipo()));
+		
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
+				objDto.getBairro(), objDto.getCep(), cli, cid);
+		
+		cli.getEnderecos().add(end);
+		
+		cli.getTelefones().add(objDto.getTel1());
+		
+		if(objDto.getTel2()!=null) {
+			cli.getTelefones().add(objDto.getTel2());
+		}
+		
+		if(objDto.getTel3()!=null) {
+			cli.getTelefones().add(objDto.getTel3());
+		}
+		
+		return cli;
+	}
+	
 	public Cliente fromDto(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
 	}
