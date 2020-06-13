a85c9675a49b3fba5d22147d8a7cfd6a4685c52a
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-13 12:41:02 -0300

    autorizando endpoints para perfis especificos

--- src/main/java/com/jardim/resources/CategoriaResource.java
+++ src/main/java/com/jardim/resources/CategoriaResource.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,63 +26,64 @@ import com.jardim.dto.CategoriaDTO;
 import com.jardim.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
-	@GetMapping(value="/{id}")
+
+	@GetMapping(value = "/{id}")
 	public ResponseEntity<Categoria> listarPorId(@PathVariable Integer id) {
 		Categoria obj = service.buscarPorId(id);
-		
+
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> listar() {
 		List<Categoria> list = service.buscar();
-		//percorrendo a lista de categorias e transpondo-a para uma lista de categoriasdto
+		// percorrendo a lista de categorias e transpondo-a para uma lista de
+		// categoriasdto
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping
-	public ResponseEntity<Void> inserir(@Valid @RequestBody CategoriaDTO objDto){
+	public ResponseEntity<Void> inserir(@Valid @RequestBody CategoriaDTO objDto) {
 		Categoria obj = service.fromDto(objDto);
 		obj = service.insert(obj);
-		//aparentemente gera o código http 201 (created) e gera a url do novo recurso criado
-		URI uri = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(obj.getId()).toUri();
+		// aparentemente gera o código http 201 (created) e gera a url do novo recurso
+		// criado
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
-	
-	@PutMapping(value="/{id}")
-	public ResponseEntity<Void> atualiza(@PathVariable Integer id, @Valid @RequestBody CategoriaDTO objDto){
-		
+
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> atualiza(@PathVariable Integer id, @Valid @RequestBody CategoriaDTO objDto) {
+
 		Categoria obj = service.fromDto(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
-	@DeleteMapping(value="/{id}")
+
+	@PreAuthorize("hasAnyRole('ADMIN')") 
+	@DeleteMapping(value = "/{id}")
 	public ResponseEntity<Void> deletarPorId(@PathVariable Integer id) {
 		service.delete(id);
-		
+
 		return ResponseEntity.noContent().build();
 	}
-	
-	//rota = /categorias/page?page=0&linesPerPage=20...
-	@GetMapping(value="/page")
-	public ResponseEntity<Page<CategoriaDTO>> listarPage(
-			@RequestParam(value="page", defaultValue="0") Integer page, 
-			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
-			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
-			@RequestParam(value="direction", defaultValue="ASC") String direction) {
-		
+
+	// rota = /categorias/page?page=0&linesPerPage=20...
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<CategoriaDTO>> listarPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
 		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listDto);
