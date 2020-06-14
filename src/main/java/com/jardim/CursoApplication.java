d600a4db60bd7b34001c8df06670b9eb146031f5
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-14 14:05:32 -0300

    salvando arquivo no s3

--- src/main/java/com/jardim/CursoApplication.java
+++ src/main/java/com/jardim/CursoApplication.java
@@ -1,13 +1,18 @@
 package com.jardim;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jardim.services.S3Service;
+
 @SpringBootApplication
 public class CursoApplication implements CommandLineRunner { // interface que permite executar códigos ao rodar esta
 														     // classe
-
+	@Autowired
+	private S3Service s3service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
 	}
@@ -15,7 +20,7 @@ public class CursoApplication implements CommandLineRunner { // interface que pe
 	// método que roda ao iniciar essa classe
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3service.uploadFile("D:\\Unisantos\\índice.png");
 	}
 
 }
