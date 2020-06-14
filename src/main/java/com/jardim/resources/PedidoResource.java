672760221bdd11f224d4b18c6acc8df70fc44f86
Gabriel Jardim <gabrielrlj99@outlook.com>
2020-06-14 10:41:46 -0300

    restrição de conteudo, cliente só recupera seus pedidos

--- src/main/java/com/jardim/resources/PedidoResource.java
+++ src/main/java/com/jardim/resources/PedidoResource.java
@@ -1,24 +1,22 @@
 package com.jardim.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.jardim.domain.Categoria;
 import com.jardim.domain.Pedido;
-import com.jardim.dto.CategoriaDTO;
 import com.jardim.services.PedidoService;
 
 @RestController
@@ -44,6 +42,16 @@ public class PedidoResource {
 				.path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<Pedido>> listarPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "instante") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+
+		Page<Pedido> list = service.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list);
 	}
 	
 
--- src/main/java/com/jardim/services/PedidoService.java
+++ src/main/java/com/jardim/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jardim.domain.Cliente;
 import com.jardim.domain.ItemPedido;
 import com.jardim.domain.PagamentoComBoleto;
 import com.jardim.domain.Pedido;
@@ -14,6 +18,8 @@ import com.jardim.domain.enums.EstadoPagamento;
 import com.jardim.repositories.ItemPedidoRepository;
 import com.jardim.repositories.PagamentoRepository;
 import com.jardim.repositories.PedidoRepository;
+import com.jardim.security.UserSS;
+import com.jardim.services.exceptions.AuthorizationException;
 import com.jardim.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -64,5 +70,17 @@ public class PedidoService {
 		itemPedidoRepo.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.buscarPorId(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
